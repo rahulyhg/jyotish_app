ef1b66bfba5c257636b8a7a7d89bf36b3b26d836
Oleg Lustenko <diethxbye@gmail.com>
2016-03-08 22:32:56 +0200

    packages

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 npm i  -D koa-favicon koa-static koa-logger koa-bodyparser koa-generic-session 
-npm i  -D koa-views jade koa-router config co-busboy koa-passport passport-local cookies socket.io socket.io-emitter socket.io-redis
-npm i  -D passport-facebook passport-vkontakte
-
+npm i  -D koa-views jade koa-router config co-busboy koa-passport passport-local cookies passport-facebook passport-vkontakte
